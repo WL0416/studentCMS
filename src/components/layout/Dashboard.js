cf17337e557337c3d62df7b56be7835c27abb8b9
WEI LI <liwsuper7@gmail.com>
2020-07-20 23:05:26 +1000

    using react-boostrap refactory

--- src/components/layout/Dashboard.js
+++ src/components/layout/Dashboard.js
@@ -1,26 +1,27 @@
 import React from "react";
 import Courses from "../courses/Courses";
 import Sidebar from "../layout/Sidebar";
+import { Col, Row } from "react-bootstrap";
 
 function Dashboard() {
   return (
-    <div className="row">
+    <Row>
       <div className="col-md-10 mb-5">
-        <div className="row">
-          <div className="col-md-6">
+        <Row>
+          <Col md={6}>
             <h2>
               <i className="fas fa-graduation-cap"></i> Courses
             </h2>
-          </div>
-        </div>
+          </Col>
+        </Row>
         <Courses />
       </div>
-      <div className="col-md-2">
+      <Col md={2}>
         <br />
         <br />
         <Sidebar />
-      </div>
-    </div>
+      </Col>
+    </Row>
   );
 }
 export default Dashboard;
